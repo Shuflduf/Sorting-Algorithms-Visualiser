ec08238361a363bf47c84d603299c9df562cde09
Shuflduf <shuflduf@gmail.com>
2024-08-20 14:19:25 -0600

    swapping bars

--- main.gd
+++ main.gd
@@ -5,21 +5,45 @@ extends Control
 
 var max_size: int
 
+func get_max_size() -> int:
+	await get_tree().process_frame
+	var max = %Bar.size.y
+	%Bar.queue_free()
+	return max
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().process_frame
-	max_size = %Bar.size.y
-	%Bar.queue_free()
+	max_size = await get_max_size()
 	for i in count:
 		var height = float(i + 1) / float(count)
 		var new_bar: Panel = bar_scene.instantiate()
-		print(height)
 		%Bars.add_child(new_bar)
 		new_bar.custom_minimum_size.y = height * max_size
-		#new_bar.size_flags_vertical = Control.SIZE_SHRINK_END
 
+	await swap_bars(2, 4)
+	await swap_bars(1, 3)
+	await swap_bars(1, 0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func swap_bars(first: int, second: int):
+	await get_tree().process_frame
 
-	pass
+	var first_bar = %Bars.get_child(first)
+	var second_bar = %Bars.get_child(second)
+
+	var first_pos = first_bar.position.x
+	var second_pos = second_bar.position.x
+	print(first_pos, ", ", second_pos)
+
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC)
+	tween.parallel().tween_property(first_bar, "position:x", second_pos, 0.5)
+	tween.parallel().tween_property(second_bar, "position:x", first_pos, 0.5)
+	await tween.finished
+
+	%Bars.move_child(first_bar, second)
+	%Bars.move_child(second_bar, first)
+
+	return
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		print(event.global_position)
