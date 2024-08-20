60f46b8d8d0534b10f189be6785fcb5a5a9ac8b6
Shuflduf <shuflduf@gmail.com>
2024-08-20 14:24:13 -0600

    shuffling

--- main.gd
+++ main.gd
@@ -7,9 +7,9 @@ var max_size: int
 
 func get_max_size() -> int:
 	await get_tree().process_frame
-	var max = %Bar.size.y
+	var m = %Bar.size.y
 	%Bar.queue_free()
-	return max
+	return m
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,29 +20,36 @@ func _ready() -> void:
 		%Bars.add_child(new_bar)
 		new_bar.custom_minimum_size.y = height * max_size
 
-	await swap_bars(2, 4)
-	await swap_bars(1, 3)
-	await swap_bars(1, 0)
+	shuffle_bars()
+	#await swap_bars(2, 4)
+	#await swap_bars(1, 3)
+	#await swap_bars(1, 0)
 
-func swap_bars(first: int, second: int):
+func swap_bars(first: int, second: int, fast = false):
 	await get_tree().process_frame
 
 	var first_bar = %Bars.get_child(first)
 	var second_bar = %Bars.get_child(second)
 
-	var first_pos = first_bar.position.x
-	var second_pos = second_bar.position.x
-	print(first_pos, ", ", second_pos)
+	if !fast:
+		var first_pos = first_bar.position.x
+		var second_pos = second_bar.position.x
+		print(first_pos, ", ", second_pos)
 
-	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC)
-	tween.parallel().tween_property(first_bar, "position:x", second_pos, 0.5)
-	tween.parallel().tween_property(second_bar, "position:x", first_pos, 0.5)
-	await tween.finished
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_CIRC)
+		tween.parallel().tween_property(first_bar, "position:x", second_pos, 0.5)
+		tween.parallel().tween_property(second_bar, "position:x", first_pos, 0.5)
+		await tween.finished
 
 	%Bars.move_child(first_bar, second)
 	%Bars.move_child(second_bar, first)
 
 	return
+
+func shuffle_bars():
+	randomize()
+	for i in count * 10:
+		%Bars.move_child(%Bars.get_child(randi_range(0, count)), randi_range(0, count))
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
