329a1ef70871502676f6e06b27fdeb3c3ff9d807
Shuflduf <shuflduf@gmail.com>
2024-08-20 18:51:57 -0600

    gnome sort

---
+++ gnome.gd
@@ -0,0 +1,16 @@
+extends Sorter
+
+
+func sort():
+	var pos = 0
+	while !owner.sorted():
+		#await get_tree().process_frame
+		#for i in %Bars.get_child_count() - 1:
+		var bar = %Bars.get_child(pos)
+		var prev_bar = %Bars.get_child(pos - 1)
+
+		if pos == 0 or bar.custom_minimum_size.y >= prev_bar.custom_minimum_size.y:
+			pos += 1
+		else:
+			await owner.swap_bars(pos, pos - 1)
+			pos -= 1
